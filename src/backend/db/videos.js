fee8ce463fb16b97e410321d957598b84c7dba9b
Manpreet <manpreetisar@gmail.com>
2022-05-08 01:33:21 +0530

    add video backend database & index file

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,139 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "uEqYzdz3Zvg",
+    title: "Exquisite Afternoon Raag Bhimpalasi | Kaushiki Chakraborty |",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Kaushiki’s virtuoso technique is the result of years of intense training, primarily from her esteemed father Pandit Ajoy Chakraborty. She hails from the Patiala gharana, known for a strong Islamic influence and elaborate ornamentation when singing taan [fast improvised melodies]. She describes her approach: “It’s all in the mind. One has to mentally align oneself to the form...My training has taught me two approaches to music – to do music and or to become it. The latter gives you the fluidity to transcend form and become one with it.” ",
+    views: "303k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/uEqYzdz3Zvg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA4TuIB_olQjs7Zpi7PUdYPKTIkRg",
+    category: "Classical",
+    uploaded: "Mar 29, 2020",
+  },
+
+  {
+    _id: "YLkdm2pe8hA",
+    title: "B Praak | Crossblade Live | Gurnazar | Dholna | Robby Singh",
+    description:
+      "B Praak | Crossblade Live | Gurnazar | Dholna | Robby Singh | 2019” ",
+    views: "503k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/YLkdm2pe8hA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBybgMYjb4YCndMl3ePzul1sld9lA",
+    category: "Classical",
+    uploaded: "Mar 05, 2019",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "xfA6ySYiuE4",
+    title: "Kabira Naina | Mohd Irfan | Neha Kakka ",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Mixtape: Kabira Naina | Mohd Irfan | Neha Kakka | Bhushan Kumar Abhijit V",
+    views: "889k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/xfA6ySYiuE4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLApnicigkMNQfybzYn09U7MIoGbJw",
+    category: "Classical",
+    uploaded: "May 27, 2018",
+  },
+  {
+    _id: "d8Ps9QvTT_w",
+    title:
+      "Indian classical sensations Kaushiki Chakraborty and Mahesh Kale | Sur Jyotsna Awards 2019 |",
+    description:
+      "Indian classical sensations Kaushiki Chakraborty and Mahesh Kale entertained the music lovers with their marvellous acts during the event. First time ever – two classical performers from different musical gharanas came together to perform at this event. The duo’s beautiful musical chemistry reflected in their jugalbandi on Kanada Raja Pandharicha which took the crowd to a different musical journey and gave a shining end to the evening. ",
+    views: "383k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/d8Ps9QvTT_w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFnPmzkQylxw6I9mE9R3qdvIlVUA",
+    category: "Classical",
+    uploaded: "Apr 16, 2019",
+  },
+  {
+    _id: "28hYUZMufDg",
+    title: "DaBaby - BOP on Broadway (Hip Hop Musical)",
+    description:
+      "Shoutout every single dancer that came out to be great.Shout Cherry and Danileigh for the Choreography. ",
+    views: "203k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/28hYUZMufDg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB3Bax0rQ1_VL9wnLRFgn-T2GzHxg",
+    category: "Hip-hop",
+    uploaded: "Apr 16, 2018",
+  },
+  {
+    _id: "FsC9gGQ37-4",
+    title: "ELECTRO FOLK: BHUMBRO | Shirley Setia, Parry G & Aditya Dev ",
+    description:
+      "Presenting the third video song  Bhumbro sung by Shirley Setia, Parry G & Aditya Dev from Electro Folk Series.",
+    views: "309k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/FsC9gGQ37-4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAPHtH2XxMdZlkHM9kk205jGpVCzQ",
+    category: "Folk",
+    uploaded: "Jan 17, 2019",
+  },
+  {
+    _id: "jy3mTnTsojo",
+    title: "AJJ KAL AJJ KAL (Official Video) Nimrat Khaira|| Punjabi song",
+    description:
+      "AJJ KAL AJJ KAL (Official Video) Nimrat Khaira | Bunty Bains | Desi Crew | Latest Punjabi Songs 2020",
+    views: "509k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/jy3mTnTsojo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC3pSHTnqyM34vZablqcTBw0S7JTA",
+    category: "Folk",
+    uploaded: "Jun 11, 2020",
+  },
+
+  {
+    _id: "DZC7zzgtPwM",
+    title:
+      "Titliaan Warga | Harrdy Sandhu ft Jaani | Sargun Mehta | Arvindr Khaira | Avvy Sra",
+    description:
+      "After the Massive Hit success of song Titliaan  Desi Melodies Proudly  Presents TITLIAAN WARGA  an interesting take on a heart break story  again featuring Harrdy Sandhu Ft Jaani  and Sargun Mehta. This Time it's sung  By super Melodies singer Harrdy Sandhu alongside Jaani with his Shayri  Flavour  its music has been done by Avvy Sra. The deep meaning of the lyrics are written by none other than JAANI and this new conceptual video has been directed by ace video director Arvindr Khaira.",
+    views: "603k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/DZC7zzgtPwM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCrOysHtj8xHo7wpY9i-wjWoXtxnQ",
+    category: "Cover",
+    uploaded: "Jan 6, 2021",
+  },
+  {
+    _id: "JuduiEAkwZc",
+    title: "Tere bina | Cover by Noor Chahal & Kushagra Thakur",
+    description: "Collaborated with the extremely talented Kushagra Thakur ",
+    views: "600k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/JuduiEAkwZc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzKa4hzMgz0GqC5dxZ5qVzO_4rQw",
+    category: "Cover",
+    uploaded: "Feb 9, 2021",
+  },
+  {
+    _id: "h8L2JfZuIxE",
+    title: "Tich Button | Punjabi Cover Song - Simar Sethi",
+    description:
+      "Tich Button | Chahida kuch vi nai Punjabi Song - Simar Sethi | New Punjabi Song 2021 | New Song 2022 ",
+    views: "400k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/h8L2JfZuIxE/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCaAaeVr96b7X_iFIm63Z57k4eFNA",
+    category: "Cover",
+    uploaded: "Oct 22, 2021",
+  },
+
+  {
+    _id: "5rX8m2mlQEA",
+    title:
+      "Atif Aslam's Heart Touching Performance Live at Star GIMA Awards 2015 Full HD Video",
+    description:
+      "Atif Aslam's Heart Touching Performance   Live at Star GIMA Awards 2015   Full HD Video",
+    views: "653k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/5rX8m2mlQEA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBpOkL8PbJuR9654SF2FEZ75qqaaA",
+    category: "Live",
+    uploaded: "Jan 2, 2020",
+  },
+  {
+    _id: "8rjPMMatzFI",
+    title: "College talent show || Live performance",
+    description: "College talent show || Live performance",
+    views: "523k views",
+    thumbnail:
+      "https://i.ytimg.com/vi/8rjPMMatzFI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAKPsNhzAeixYysPK1TLKONrJDFeA",
+    category: "Live",
+    uploaded: "Nov 21, 2019",
   },
 ];
