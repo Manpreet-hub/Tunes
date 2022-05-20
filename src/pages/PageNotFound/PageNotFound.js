2ea6e9e7ef0ffda1c0c297ce3b1d5971502aa1eb
Manpreet <manpreetisar@gmail.com>
2022-05-20 23:36:52 +0530

    added alt

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -3,7 +3,7 @@ import "./pagenotFound.css";
 export const PageNotFound = () => {
   return (
     <div className="pageNotFound-img">
-      <img src={pageNotFound} alt="" width="760px" />
+      <img src={pageNotFound} alt="404-pageNotFound" width="760px" />
     </div>
   );
 };
