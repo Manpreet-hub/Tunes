1c7d61c8b682828e7eb52ab3e0c8d67b6e5ad472
Manpreet <manpreetisar@gmail.com>
2022-05-22 16:52:40 +0530

    add horizontal card component

---
+++ src/components/HorizontalVideoCard/HorizontalVideoCard.js
@@ -0,0 +1,34 @@
+import "./horizontalVideoCard.css";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import { Link } from "react-router-dom";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+export const HorizontalVideoCard = ({ video, onClick }) => {
+  const { _id, title, views, thumbnail, uploaded } = video;
+
+  return (
+    <div className="horizontal-card">
+      <Link to={`/video/${_id}`}>
+        <img
+          className="img"
+          src={thumbnail}
+          width="50px"
+          height="120px"
+          alt="video-thumbnail"
+        />
+      </Link>
+      <div className="video-heading horizontal-container">
+        <div className="video-title">{title}</div>
+        <div className="icon-section">
+          <CancelIcon className="delete-icon" onClick={onClick} />
+        </div>
+        <div className="horizontal-video-footer">
+          <div className=" p para-md">{views} ·</div>
+          <span className="p para-md">{uploaded}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
