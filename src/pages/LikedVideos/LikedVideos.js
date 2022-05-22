a1fdd804b00197f11d912af98f033c058a1650a1
Manpreet <manpreetisar@gmail.com>
2022-05-22 16:47:06 +0530

    feat: like/Dislike

--- src/pages/LikedVideos/LikedVideos.js
+++ src/pages/LikedVideos/LikedVideos.js
@@ -1,3 +1,34 @@
+import { useData } from "../../context/";
+import { removeFromLike } from "../../services/";
+import { HorizontalVideoCard } from "../../components";
+import { Link } from "react-router-dom";
+
 export const LikedVideos = () => {
-  return <h2>LikedVideos page</h2>;
+  const { dataState, dataDispatch } = useData();
+  return (
+    <>
+      {dataState.likes.length === 0 ? (
+        <div className="page-container">
+          <h3>
+            Like Page is empty. Please watch and like videos to see them here!
+          </h3>
+          <button class="btn-default btn-primary">
+            <Link to="/">Watch Now</Link>
+          </button>
+        </div>
+      ) : (
+        <div className="listing-vertical">
+          {dataState.likes.map((video) => {
+            return (
+              <HorizontalVideoCard
+                video={video}
+                key={video._id}
+                onClick={() => removeFromLike(dataDispatch, video._id)}
+              />
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
 };
---
+++ src/services/likeVideoService.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+export const addToLikeVideo = async (dataDispatch, video) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const token = localStorage.getItem("token");
+
+    try {
+      const {
+        data: { likes },
+      } = await axios.post(
+        "/api/user/likes",
+        { video },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      dataDispatch({ type: "ADD_TO_LIKE", payload: likes });
+    } catch (error) {
+      console.log(error);
+    }
+  } else alert("Please login first");
+};
+
+export const removeFromLike = async (dataDispatch, videoId) => {
+  console.log(videoId);
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    const token = localStorage.getItem("token");
+
+    try {
+      const {
+        data: { likes },
+      } = await axios.delete(`/api/user/likes/${videoId}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      dataDispatch({
+        type: "REMOVE_FROM_LIKE",
+        payload: likes,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else alert("Please login first");
+};
