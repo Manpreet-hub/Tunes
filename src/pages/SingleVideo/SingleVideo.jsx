2b0d08154e8c57ad80eecdc384bfb2fbb3237774
Manpreet <manpreetisar@gmail.com>
2022-05-22 16:58:15 +0530

    add react player ,liked functionality

--- src/pages/SingleVideo/SingleVideo.jsx
+++ src/pages/SingleVideo/SingleVideo.jsx
@@ -6,52 +6,90 @@ import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import { VideoCard } from "../../components/";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ReactPlayer from "react-player";
+import { useAuth, useData } from "../../context/";
+import { addToLikeVideo, removeFromLike, postHistory } from "../../services/";
 
 export const SingleVideo = () => {
+  const { dataState, dataDispatch } = useData();
+  const { likes } = dataState;
+  const navigate = useNavigate();
   let { videoId } = useParams();
-  const video = videos.find((product) => product._id === videoId);
-  console.log(video);
-  const { _id, title, views, uploaded } = video;
-  return (
-    <>
-      <div className="single-video-container">
-        <div className="video-iframe">
-          <iframe
-            title={title}
-            src={`https://www.youtube.com/embed/${video?._id}`}
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
 
-          <div className="vid-title">{title}</div>
-          <div className="video-info">
-            <div className="flex-row ">
-              <p>
-                {views} · {uploaded}
-              </p>
-            </div>
-            <div className="icons">
+  const [video, setVideo] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(`/api/video/${videoId}`);
+        setVideo(response.data.video);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [videoId, video]);
+
+  const isVideoLiked = () => {
+    return likes.some((likedVideo) => likedVideo._id === video._id);
+  };
+
+  const {
+    authState: { isAuthenticated },
+  } = useAuth();
+
+  return (
+    <div className="single-video-container">
+      <div className="video-iframe">
+        <ReactPlayer
+          width="100%"
+          height="20%"
+          controls
+          url={`https://www.youtube.com/embed/${video?._id}`}
+          onStart={() => postHistory(dataDispatch, video)}
+        />
+
+        <div className="vid-title">{video.title}</div>
+        <div className="video-info">
+          <div className="flex-row ">
+            <p>
+              {video.views} · {video.uploaded}
+            </p>
+          </div>
+          <div className="icons">
+            {!isVideoLiked() ? (
               <span>
-                <ThumbUpOutlinedIcon className="icon" />
+                <ThumbUpOutlinedIcon
+                  className="icon"
+                  onClick={() => addToLikeVideo(dataDispatch, video)}
+                />
                 LIKE
               </span>
+            ) : (
               <span>
-                <ShareOutlinedIcon className="icon" />
-                SHARE
+                <ThumbUpOutlinedIcon
+                  className="icon"
+                  onClick={() => removeFromLike(dataDispatch, video._id)}
+                />
+                LIKED
               </span>
-              <span>
-                <PlaylistAddIcon className="icon" /> SAVE
-              </span>
-            </div>
+            )}
+            <span>
+              <ShareOutlinedIcon className="icon" />
+              SHARE
+            </span>
+            <span>
+              <PlaylistAddIcon className="icon" /> SAVE
+            </span>
           </div>
         </div>
-        <div className="side-videos">
-          {videos.map((video) => (
-            <VideoCard key={video._id} videoInfo={video} />
-          ))}
-        </div>
       </div>
-    </>
+      <div className="side-videos">
+        {videos.map((video) => (
+          <VideoCard key={video._id} videoInfo={video} />
+        ))}
+      </div>
+    </div>
   );
 };
