bd5961519f9a0763b895cebc419e3182ba5f773e
Manpreet <manpreetisar@gmail.com>
2022-05-22 16:48:30 +0530

    feat: watch history functionality

---
+++ src/services/historyService.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+
+const getHistory = async (dataDispatch) => {
+  const token = localStorage.getItem("token");
+
+  try {
+    const {
+      data: { history },
+    } = await axios.get(`/api/user/history`, {
+      headers: {
+        authorization: token,
+      },
+    });
+    dataDispatch({ type: "SET_HISTORY", payload: history });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const postHistory = async (dataDispatch, video) => {
+  const token = localStorage.getItem("token");
+
+  try {
+    console.log("post history");
+    const {
+      data: { history },
+    } = await axios.post(
+      `/api/user/history`,
+      { video },
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+    dataDispatch({ type: "SET_HISTORY", payload: history });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteHistory = async (dataDispatch, videoId) => {
+  const token = localStorage.getItem("token");
+
+  try {
+    const {
+      data: { history },
+    } = await axios.delete(`/api/user/history/${videoId}`, {
+      headers: { authorization: token },
+    });
+
+    dataDispatch({ type: "SET_HISTORY", payload: history });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const clearAllHistory = async (dataDispatch) => {
+  const token = localStorage.getItem("token");
+
+  try {
+    const {
+      data: { history },
+    } = await axios.delete(`/api/user/history/all`, {
+      headers: { authorization: token },
+    });
+    dataDispatch({ type: "SET_HISTORY", payload: history });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getHistory, postHistory, deleteHistory, clearAllHistory };
